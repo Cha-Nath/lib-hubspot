87a25e8c16ec97fe6a968a72d9f0f7bec620ddfd
Nathan Chanteux <nathan@eteamsys.lu>
2020-03-10 14:27:38 +0100

    update owner

--- Classes/Owner.php
+++ Classes/Owner.php
@@ -7,8 +7,14 @@ use nlib\Hubspot\Interfaces\OwnerInterface;
 
 class Owner extends Hubspot implements OwnerInterface, HubspotInterface {
 
-    public function getOwners() {
-
-        return json_decode($this->cURL('http://api.hubapi.com/owners/v2/owners')->get($this->getHapikeys()));
+    public function getOwner(int $id, array $options = []) {
+        $owner = $this->cURL('http://api.hubapi.com/owners/v2/owners/' . $id . '?' . $this->getHapikey())->get($options);
+        return json_decode($owner);
     }
+
+    public function getOwners(array $options = []) {
+        $owners = $this->cURL('http://api.hubapi.com/owners/v2/owners?' . $this->getHapikey())->get($options);
+        return json_decode($owners);
+    }
+
 }
